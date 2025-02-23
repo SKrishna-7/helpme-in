0f56cf1439905e91ca1a2c2b5f24780ed7638094
SKrishna-7 <sureshkrishna375@gmail.com>
2025-02-23 20:50:42 +0530

    nexicon update

--- src/Components/Aboutus.jsx
+++ src/Components/Aboutus.jsx
@@ -55,7 +55,7 @@ export default function Aboutus() {
    
     return (
         <div className="h-screen w-full lg:px-10  bg-white px-3 max-h-full  lg:overflow-hidden">
-            <h1 className="text-2xl pb-6 text-left pt-20 text-orange-600">Our Services</h1>
+            <h1 className="text-2xl pb-6 text-left pt-20 text-orange-600 overflow-hidden">Our Services</h1>
       <div className=" h-full [perspective:1000px]  mt-1 flex ">
         <motion.div 
         variants={fadeIn("left",0.2)}
